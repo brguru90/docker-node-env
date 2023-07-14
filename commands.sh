6cf5158a667d7ce24d10f980aa4ec1ecd76d1168
Guruprasad BR <brguru90@gmail.com>
2023-07-14 15:55:32 +0530

    .

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-yarn create-next-app ./nextjs-practise-2023 --use-yarn --js --eslint --app --src-dir --no-tailwind --no-import-alias "@/*"
+npx create-next-app nextjs-practise-2023 ./ --use-yarn --js --eslint --app --src-dir --no-tailwind --no-import-alias "@/*"
